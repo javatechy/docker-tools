f9a06a5adca13166b2b572143a9ec5d51c871c30
javatechy <deepak@192.168.1.63>
2018-08-30 13:15:09 +0530

    Updated jkenis

--- jenkins/dokr.py
+++ jenkins/dokr.py
@@ -69,6 +69,7 @@ def cleanAll():
 def helper():
     print('\n------------------------ Command Options ------------------------')
     print('\ndokr clean searchPattern # delete old images matching pattern')
+    print('\ndokr clean-all searchPattern # clean docker')
     print('\ndokr lecs # ECS login')
     print('\ndokr tag searchPattern tagName # add a tag *tagName*on a image matching *searchPattern* ')
     print('\ndokr push searchPattern  # push all images matching the pattern')
